369780059bf9d739d2bbbada6074a8a6e316be15
Jonathan Souza <jsouza@mcontigo.com>
2024-08-12 09:58:34 -0300

    src/main/resources/

---
+++ src/main/java/br/com/customer/orders/CustomerOrdersApplication.java
@@ -0,0 +1,13 @@
+package br.com.customer.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOrdersApplication.class, args);
+	}
+
+}
